5c29a769eab66fe1746eb9abff8f128720b72127
terence tan <terence@bigbadrobots.com>
2019-04-08 16:50:22 +0800

    dded a URL for creating a new list via POST

--- python/goat/lists/tests.py
+++ python/goat/lists/tests.py
@@ -11,25 +11,7 @@ class HomePageTest(TestCase):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
 
-	def test_can_save_a_POST_request(self):
-		self.client.post('/', data={'item_text': 'A new list item'})
-
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new list item')
-
-	def test_redirects_after_post(self):
-		response = self.client.post('/', data={'item_text': 'A new list item'})
-
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
-
-	def test_only_saves_items_when_necessary(self):
-		self.client.get('/')
-		self.assertEqual(Item.objects.count(), 0)
-
 class ItemModelTest(TestCase):
-
 	def test_saving_and_retrieving_items(self):
 		first_item = Item()
 		first_item.text = 'The first (ever) list item'
@@ -51,7 +33,7 @@ class LiveViewTest(TestCase):
 	def test_uses_list_template(self):
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		self.assertTemplateUsed(response, 'list.html')
-		
+
 	def test_displays_all_list_items(self):
 		Item.objects.create(text='itemy 1')
 		Item.objects.create(text='itemy 2')
@@ -60,4 +42,16 @@ class LiveViewTest(TestCase):
 
 		self.assertContains(response, 'itemy 1')
 		self.assertContains(response, 'itemy 2')		
+
+class NewListTest(TestCase):
+	def test_can_save_a_POST_request(self):
+		self.client.post('/lists/new', data={'item_text': 'A new list item'})
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+	def test_redirects_after_post(self):
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
